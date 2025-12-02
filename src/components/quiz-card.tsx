255665c1033079f67e2a02351bb21b20822a6d56
03675135365 <renison.oliveira@serpro.gov.br>
2025-12-02 13:15:12 -0300

    alterações

--- src/components/quiz-card.tsx
+++ src/components/quiz-card.tsx
@@ -47,32 +47,33 @@ export default function QuizPage() {
   }
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-black p-4">
-      <div className="w-full max-w-md bg-gray-300 shadow-lg rounded-xl p-6 border border-gray-200">
-        {!finished ? (
-          <>
-            <h2 className="text-xl text-zinc-900 font-semibold mb-4">
-              Pergunta {index + 1} de {questions.length}
-            </h2>
+      <div className="w-full min-h-screen flex flex-col gap-7 items-center justify-center bg-black p-4">
+        <h1 className="text-4xl text-gray-300 font-bold">Caixa Preta Quiz</h1>
+        <div className="w-full max-w-md border-2 border-red-700  bg-gray-300 shadow-lg rounded-xl p-6">
+            {!finished ? (
+                <>
+                    <h2 className="text-xl text-zinc-900 font-semibold mb-4">
+                        Pergunta {index + 1} de {questions.length}
+                    </h2>
 
-            <p className="text-gray-700 mb-6">{questions[index].text}</p>
+                    <p className="text-gray-700 mb-6">{questions[index].text}</p>
 
-            <div className="space-y-3">
-              {questions[index].options.map(opt => (
-                <button
-                  key={opt}
-                  onClick={() => handleAnswer(opt)}
-                  className="w-full px-4 text-zinc-900 py-3 rounded-lg border border-gray-300 bg-gray-50 hover:bg-gray-100 active:scale-95 transition"
-                >
-                  {opt}
-                </button>
-              ))}
-            </div>
-          </>
+                    <div className="space-y-3">
+                        {questions[index].options.map(opt => (
+                            <button
+                            key={opt}
+                            onClick={() => handleAnswer(opt)}
+                            className="w-full px-4 text-zinc-900 py-3 rounded-lg border border-gray-300 bg-gray-50 hover:bg-gray-100 active:scale-95 transition"
+                            >
+                            {opt}
+                            </button>
+                        ))}
+                    </div>
+                </>
         ) : (
           <div className="text-center text-zinc-900">
             <h2 className="text-2xl font-bold mb-2">Quiz finalizado!</h2>
-            <p className=" flex gap-4">
+            <p className=" flex flex-col gap-4">
                 <div className="flex flex-col items-center m-auto gap-4">
                     <h3 className="text-lg font-medium mb-2">Suas respostas</h3>
                     <div className="flex gap-3">
@@ -81,6 +82,7 @@ export default function QuizPage() {
                         <span className="bg-white px-3 py-1 rounded-2xl">{answers.q3}</span>
                     </div>
                 </div>
+                <span>As respostas estão aparecendo apenas para fins de demonstração</span>
             </p>
           </div>
         )}
